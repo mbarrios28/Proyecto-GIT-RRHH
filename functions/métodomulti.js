120279027db1f47a88ae27a97fad874aff128715
Fernando <107973559+FernandoBarra05@users.noreply.github.com>
2023-07-28 18:41:02 -0500

    Add files via upload

---
+++ functions/métodomulti.js
@@ -0,0 +1,11 @@
+const Calculadora = {
+    multiplicar: function(a, b) {
+      return a * b;
+    }
+  };
+  
+  // Ejemplo de uso:
+  const resultado = Calculadora.multiplicar(5, 10);
+  console.log(resultado);
+  
+  
